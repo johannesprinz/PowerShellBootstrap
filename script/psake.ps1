bf008c6b39bf6b98c3be1c1c71f7645f71fd69a5
Johannes Prinz <johannesprinz@gmail.com>
2017-07-18 12:42:59 +1200

    trying to make the psake bootstrap more robust

--- script/psake.ps1
+++ script/psake.ps1
@@ -8,25 +8,35 @@ function Test-Module($Name, $MinimumVersion){
 		 [System.Version]::Parse($MinimumVersion) -le $_.Version
 	});
 }
-$requiredModules = @(
-	@{Name="PackageManagement"; MinimumVersion="1.1.4.0";},
-	@{Name="PowerShellGet"; MinimumVersion="1.1.3.1";},
-	@{Name="psake"; MinimumVersion="4.6.0";},
-	@{Name="Pester"; MinimumVersion="4.0.3";},
-	@{Name="PSScriptAnalyzer"; MinimumVersion="1.15.0";}
-)
-if($null -ne [System.Net.WebRequest]){
-	[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials;
-}
-if(-not(Test-Module -Name "PowerShellGet")) {
-	Write-Error -Message "PowerShellGet module not found. You can get it from here: https://www.powershellgallery.com/" -Category NotInstalled;
-} else {
+
+function Load-DependantModules(){
+	$dependantModules = @{
+		PackageManagement = @{Name="PackageManagement"; MinimumVersion="1.1.4.0";};
+		PowerShellGet = @{Name="PowerShellGet"; MinimumVersion="1.1.3.1";};
+	}
+	if(-not (Test-Module -Name $dependantModules.PowerShellGet.Name -MinimumVersion $dependantModules.PowerShellGet.MinimumVersion)){
+			Install-Module -Name $dependantModules.PowerShellGet.Name -MinimumVersion $dependantModules.PowerShellGet.MinimumVersion -Scope CurrentUser -Force;
+			Remove-Module -Name $dependantModules.PowerShellGet.Name;
+			Import-Module -Name  $dependantModules.PowerShellGet.Name;
+	}
+	if(-not (Test-Module -Name $dependantModules.PackageManagement.Name -MinimumVersion $dependantModules.PackageManagement.MinimumVersion)){
+			Install-Module -Name $dependantModules.PackageManagement.Name -MinimumVersion $dependantModules.PackageManagement.MinimumVersion -Scope CurrentUser -Force -SkipPublisherCheck -AllowClobber;
+			Remove-Module -Name $dependantModules.PackageManagement.Name;
+			Import-Module -Name  $dependantModules.PackageManagement.Name;
+	}
 	if(-not (Get-PackageProvider -Name NuGet)){
 		Install-PackageProvider -Name Nuget
 	}
 	if(-not (Get-PSRepository -Name PSGallery -ErrorAction SilentlyContinue)){
 		Register-PSRepository -Name PSGallery -SourceLocation https://www.powershellgallery.com/api/v2;
 	}
+}
+function Load-RequiredModules(){
+	$requiredModules = @(
+		@{Name="psake"; MinimumVersion="4.6.0";},
+		@{Name="Pester"; MinimumVersion="4.0.3";},
+		@{Name="PSScriptAnalyzer"; MinimumVersion="1.15.0";}
+	)
 	$requiredModules | ForEach-Object {
 		if(-not (Test-Module -Name $_.Name -MinimumVersion $_.MinimumVersion)){
 			Install-Module -Name $_.Name -MinimumVersion $_.MinimumVersion -Scope CurrentUser -Force -SkipPublisherCheck -AllowClobber;
@@ -37,6 +47,16 @@ if(-not(Test-Module -Name "PowerShellGet")) {
 			Import-Module -Name $_.Name;
 		}
 	}
+}
+
+if($null -ne [System.Net.WebRequest]){
+	[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials;
+}
+if(-not(Test-Module -Name "PowerShellGet")) {
+	Write-Error -Message "PowerShellGet module not found. You can get it from here: https://www.powershellgallery.com/" -Category NotInstalled;
+} else {
+	Load-DependantModules;
+	Load-RequiredModules;
 	"> Invoke-psake [Task Name]  #To execute task"
 	"> Invoke-psake [Task Name] -verbose  #To execute task with verbose logging"
 	"> Invoke-psake -docs  #To get this list of task"
