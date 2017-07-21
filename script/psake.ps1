80080c13f536caba30bf6e4caa60c863bc11ad8a
Johannes Prinz <johannesprinz@gmail.com>
2017-07-21 12:27:39 +1200

    fixed vanilla boot strapping issues

--- script/psake.ps1
+++ script/psake.ps1
@@ -9,27 +9,28 @@ function Test-Module($Name, $MinimumVersion){
 	});
 }
 
-function Load-DependantModules(){
+function Import-DependantModule(){
 	$dependantModules = @{
-		PackageManagement = @{Name="PackageManagement"; MinimumVersion="1.1.4.0";};
+		NuGet = @{Name="NuGet"; MinimumVersion="2.8.5.208";};
 		PowerShellGet = @{Name="PowerShellGet"; MinimumVersion="1.1.3.1";};
+		PackageManagement = @{Name="PackageManagement"; MinimumVersion="1.1.4.0";};
 	}
-	if(-not (Test-Module -Name $dependantModules.PowerShellGet.Name -MinimumVersion $dependantModules.PowerShellGet.MinimumVersion)){
-			Install-Module -Name $dependantModules.PowerShellGet.Name -MinimumVersion $dependantModules.PowerShellGet.MinimumVersion -Scope CurrentUser -Force;
-			Remove-Module -Name $dependantModules.PowerShellGet.Name;
-			Import-Module -Name  $dependantModules.PowerShellGet.Name;
+	if(-not (Get-PackageProvider -Name $dependantModules.NuGet.Name)){
+		Install-PackageProvider -Name $dependantModules.NuGet.Name -MinimumVersion $dependantModules.NuGet.MinimumVersion -Force
 	}
 	if(-not (Test-Module -Name $dependantModules.PackageManagement.Name -MinimumVersion $dependantModules.PackageManagement.MinimumVersion)){
 			Install-Module -Name $dependantModules.PackageManagement.Name -MinimumVersion $dependantModules.PackageManagement.MinimumVersion -Scope CurrentUser -Force -SkipPublisherCheck -AllowClobber;
 	}
-	if(-not (Get-PackageProvider -Name NuGet)){
-		Install-PackageProvider -Name Nuget
+	if(-not (Test-Module -Name $dependantModules.PowerShellGet.Name -MinimumVersion $dependantModules.PowerShellGet.MinimumVersion)){
+			Install-Module -Name $dependantModules.PowerShellGet.Name -MinimumVersion $dependantModules.PowerShellGet.MinimumVersion -Scope CurrentUser -Force -AllowClobber;
+			Remove-Module -Name $dependantModules.PowerShellGet.Name;
+			Import-Module -Name  $dependantModules.PowerShellGet.Name;
 	}
 	if(-not (Get-PSRepository -Name PSGallery -ErrorAction SilentlyContinue)){
 		Register-PSRepository -Name PSGallery -SourceLocation https://www.powershellgallery.com/api/v2;
 	}
 }
-function Load-RequiredModules(){
+function Import-RequiredModule(){
 	$requiredModules = @(
 		@{Name="psake"; MinimumVersion="4.6.0";},
 		@{Name="Pester"; MinimumVersion="4.0.3";},
@@ -53,8 +54,8 @@ if($null -ne [System.Net.WebRequest]){
 if(-not(Test-Module -Name "PowerShellGet")) {
 	Write-Error -Message "PowerShellGet module not found. You can get it from here: https://www.powershellgallery.com/" -Category NotInstalled;
 } else {
-	Load-DependantModules;
-	Load-RequiredModules;
+	Import-DependantModule;
+	Import-RequiredModule;
 	"> Invoke-psake [Task Name]  #To execute task"
 	"> Invoke-psake [Task Name] -verbose  #To execute task with verbose logging"
 	"> Invoke-psake -docs  #To get this list of task"
